58a7096dfe1466d6e6622c800edb47d212c96951
Hanish Bajaj <hanishh.bajaj@gmail.com>
2026-04-16 01:02:48 +0530

    changes made

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ console.log("DB PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
